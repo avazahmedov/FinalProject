075133fd6c9f1ad6f4891b1c3253ed392575edf3
Avaz Ahmedov <qoraqosh1993@mail.ru>
2023-01-23 22:18:12 +0500

    add method ShowArray

--- Program.cs
+++ Program.cs
@@ -7,8 +7,20 @@
         array[i] = Convert.ToString(Console.ReadLine());
     }
     return array;
+    Console.WriteLine();
 }
+
+void ShowArray(string [] array)
+{
+   for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write($" \"{array[i]}\" ");
+    } 
+}
+
+
 
 Console.WriteLine("Enter array size");
 int size = Convert.ToInt32(Console.ReadLine());
 string [] myarray = CreateArrray(size);
+ShowArray(myarray);
