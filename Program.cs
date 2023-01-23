382cc23af8d53694d4870e57a20eb276a1064d2d
Avaz Ahmedov <qoraqosh1993@mail.ru>
2023-01-23 22:06:41 +0500

    add method CreateArray

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string [] CreateArrray(int size)
+{
+    string [] array = new string [size];
+    for (int i = 0; i<size; i++)
+    {
+        Console.WriteLine("Enter array element");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.WriteLine("Enter array size");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] myarray = CreateArrray(size);
