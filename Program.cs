07549f150dadfc5fb59e62bb7f311176dc71a7c9
Avaz Ahmedov <qoraqosh1993@mail.ru>
2023-01-23 23:06:50 +0500

    add method NewArrayLess3Symbol

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     string [] array = new string [size];
     for (int i = 0; i<size; i++)
     {
-        Console.WriteLine("Enter array element");
+        Console.WriteLine("Enter array element:");
         array[i] = Convert.ToString(Console.ReadLine());
     }
     return array;
@@ -18,9 +18,33 @@ void ShowArray(string [] array)
     } 
 }
 
+string [] NewArrayLess3Symbol(string [] myarray)
+{
+    int count = 0;
+    for(int i = 0; i<myarray.Length; i++)
+    {
+        string a = myarray[i];
+        if (a.Length<=3)
+        count++;
+    }
 
+    string [] array = new string [count];
+    for (int i = 0, j = 0; i<myarray.Length; i++)
+    {
+        string a = myarray[i];
+        if (a.Length<=3)
+        {
+            array[j] = myarray[i];
+            j++;
+        }
+    }
+    return array;
+}
 
-Console.WriteLine("Enter array size");
+Console.WriteLine("Enter array size:");
 int size = Convert.ToInt32(Console.ReadLine());
 string [] myarray = CreateArrray(size);
 ShowArray(myarray);
+Console.WriteLine();
+string [] newarray = NewArrayLess3Symbol(myarray);
+ShowArray(newarray);
